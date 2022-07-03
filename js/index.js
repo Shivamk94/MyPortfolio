499836bfe741a64bb4df6f92adc902a24525785b
ACS\shivam.kavalanekar <shivam.kavalanekar@oneadvanced.com>
2022-07-03 10:53:57 +0530

    005 - Added index.js file

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+var curVersion = "The beginner level Programmer"
+var theGoal = "To become the GOD level Programmer";
+
+while(curVersion == theGoal){
+    alert("Keep Practicing!");
+    curVersion++;
+}
